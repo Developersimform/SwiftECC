b6aa6ac18ba293c8d210c70477cf3e257a68e90a
Developersimform <100925886+Developersimform@users.noreply.github.com>
2022-12-22 15:26:15 +0530

    Update dep.Package version

--- Package.swift
+++ Package.swift
@@ -17,13 +17,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/leif-ibsen/ASN1", from: "2.0.2"),
+        .package(url: "https://github.com/leif-ibsen/BigInt", from: "1.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftECC",
-            dependencies: ["ASN1"]),
+            dependencies: ["ASN1", "BigInt"),
         .testTarget(
             name: "SwiftECCTests",
             dependencies: ["SwiftECC"]),
