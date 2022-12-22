510b21eb66a10db18fc5448af1b04e42a2a2a063
Developersimform <100925886+Developersimform@users.noreply.github.com>
2022-12-22 15:13:44 +0530

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftECC",
-            dependencies: ["ASN1", "BigInt"]),
+            dependencies: ["ASN1"]),
         .testTarget(
             name: "SwiftECCTests",
             dependencies: ["SwiftECC"]),
